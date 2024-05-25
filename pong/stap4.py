e472538ea1193cc289240c6dd3c5697fc158ea71
darf53 <darf53@users.noreply.github.com>
2024-05-25 10:52:51 +0200

    Update stap4.py
    Update bal_x en bal_y

--- pong/stap4.py
+++ pong/stap4.py
@@ -10,8 +10,8 @@ BAL_STRAAL = int(BAL_DIAMETER / 2)
 KLEUR_ZWART = (0, 0, 0)
 KLEUR_WIT = (255, 255, 255)
 
-bal_x = 400
-bal_y = 300
+bal_x = int(VENSTER_BREEDTE / 2)
+bal_y = int(VENSTER_HOOGTE / 2)
 bal_x_snelheid = 2
 bal_y_snelheid = 2
 
